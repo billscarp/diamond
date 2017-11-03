2960a64de1dc35576ad7f9cf4471afb52d9b1bb8
billscarp <billscarp@gmail.com>
2017-11-02 22:52:05 -0400

    added flash success, error messages

--- app.js
+++ app.js
@@ -74,7 +74,19 @@
         secret: 'secret',
         resave: true,
         saveUninitialized: true
-      }))
+      }));
+
+      ////  FLASH ////
+      //calling flash
+   app.use(flash());   
+
+   // global variables
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
     //////  ROUTES   //////
 
