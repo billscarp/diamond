0c615f2b2d991f69ae48ae0e700e1660b297ed19
billscarp <billscarp@gmail.com>
2017-11-04 12:13:02 -0400

    working on registratiton validation

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,29 @@ const router = express.Router();
     res.render('users/register');
   });
 
+  // catch the register form
+router.post('/register', (req, res) => {
+  let.error = [];
+
+  if (req.body.password != req.body.password2){
+    errors.push({text: 'Passowrds do not match'});
+  }
+  if(req.body.password.length < 6){
+    errors.push({text: 'Passowrd must be at leave 6 characters'})
+  }
+  // passes in errors, name email and password.  If wrong, user won't need to re enter everything.
+  if(errors.length > 0){
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+
+    });
+  } else {
+    res.send('passed');
+  }
+});
+
 module.exports = router;
