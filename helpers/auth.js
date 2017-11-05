ed5e54642aa1bc40e09a2126c6194b34b890ffd2
billscarp <billscarp@gmail.com>
2017-11-04 22:18:38 -0400

    auth protect routes

---
+++ helpers/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+        if(req.isAuthenticated()){
+            return next();
+        }
+        req.flash(err_msg, 'Not Authorized');
+        res.redirect('/users/login');
+    }
+}
